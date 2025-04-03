b3f95082994cb55e3224b3dbf68b5b06e40c230b
Pedro Vélez <pedrolo.pv@gmail.com>
2025-04-03 19:05:55 +0000

    finish

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-console.log("Cualquier string")
-
+console.log("Cualquier string");
 
 const pronoun = ['the', 'our'];
 const adj = ['great', 'big'];
 const noun = ['jogger', 'racoones'];
-const extension = [".com", ".net", ".us", ".io", ".es"]
-
-
+const extension = [".com", ".net", ".us", ".io", ".es"];
 
 const combinations = [];
+
 pronoun.forEach(pronoun => {
     adj.forEach(adj => {
         noun.forEach(noun => {
             extension.forEach(extension => {
+                if (noun === 'racoones' && extension === '.es') {
+                    noun = 'racoon';
+                }
                 combinations.push(`${pronoun}${adj}${noun}${extension}`);
             });
         });
     });
 });
 
-
-console.log(combinations)
+console.log(combinations);
